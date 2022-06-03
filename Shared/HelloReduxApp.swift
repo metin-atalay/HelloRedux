167385e2a480fb6f60d60f3cf234aec0c7dbd506
Metin Atalay <metin.atalay@trtworld.com>
2022-06-03 09:35:26 +0300

    Initial Commit

---
+++ Shared/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  Shared
+//
+//  Created by Metin Atalay on 3.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
